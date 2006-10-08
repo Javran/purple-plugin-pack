45dcdceb702b9dfc1cab206bc73d2a117aebefca
jbailey <devnull@localhost>
2006-10-07 23:28:25 -0400

    [pp-cvs @ 94] Since Sean added a global buddy icon selector to gaim itself, the "Compleat Buddy Icon" menu item is useless.  Kill it off.

--- bit/bit.c
+++ bit/bit.c
@@ -92,91 +92,15 @@ blist_iterate_action(gboolean remove)
 }
 
 static void
-compleat_buddy_icon_set(void *user_data, const char *filename)
-{
-	char *filedata;
-	size_t size;
-	GError *err = NULL;
-	GList *l = NULL;
-
-	gaim_debug_info("bit","Filename %s\n", filename);
-	if (!g_file_get_contents(filename, &filedata, &size, &err)) {
-		char *str;
-
-		str = g_strdup_printf(_("The following error has occurred loading %s: %s"),
-								 filename, err->message);
-		gaim_debug_info("bit", str);
-		gaim_notify_error(bit, _("Compleat Buddy Icon"), 
-						  _("Failed to load image"), str);
-
-		g_error_free(err);
-		g_free(str);
-
-		return;
-	}
-	for (l = gaim_accounts_get_all(); l != NULL; l = l->next) {
-		GaimAccount *tmp = l->data;
-		GaimBuddyIcon *icon;
-		GaimBuddy *buddy;
-		GaimConversation *conv = NULL;
-		char *username = tmp->username;
-
-		icon = gaim_buddy_icon_new(tmp, username, filedata, size);
-		gaim_debug_info("bit","Setting account %s to %s\n",username, filename);
-		gaim_account_set_buddy_icon(tmp, filename);
-		buddy = gaim_find_buddy(tmp, username);
-		if (gaim_account_is_connected(gaim_buddy_get_account(buddy)))
-			gaim_blist_update_buddy_icon(buddy);
-		conv = gaim_find_conversation_with_account(GAIM_CONV_TYPE_IM,
-		                      username,gaim_buddy_get_account(buddy));
-		if (conv != NULL)
-			gaim_conversation_update(conv, GAIM_CONV_UPDATE_ICON);
-	}
-	g_free(filedata);
-}
-
-static void
-compleat_buddy_icon_request_cb(void *user_data, const char *filename)
-{
-	FILE *import = fopen(filename, "r");
-
-	if(import) {
-		fclose(import);
-		compleat_buddy_icon_set(user_data, filename);
-	} else {
-		char *str;
-		str = g_strdup_printf(_("Cannot open file %s"),
-								 filename ? filename : "NULL");
-		gaim_debug_info("bit", str);
-		gaim_notify_error(bit, _("Compleat Buddy Icon"),
-						  _("Failed to open file"), str);
-	}
-	return;
-}
-
-static void
-compleat_buddy_icon_action(GaimPluginAction *action)
-{
-	if(gaim_accounts_get_all()) {
-		gaim_request_file(bit, _("Set Your Buddy Icon"), NULL, FALSE,
-				G_CALLBACK(compleat_buddy_icon_request_cb), NULL, NULL);
-	} else {
-		gaim_debug_info("bit", "Set Buddy Icon: No accounts defined.\n");
-		gaim_notify_error(bit, _("Set Buddy Icon"), _("No accounts defined"),
-						  _("Is there much point to setting your buddy icon when you haven't even set up an account?"));
-	}
-}
-
-static void
 flush_buddy_icon_action(GaimPluginAction *action)
 {
-blist_iterate_action(TRUE);
+	blist_iterate_action(TRUE);
 }
 
 static void
 refresh_buddy_icon_action(GaimPluginAction *action)
 {
-blist_iterate_action(FALSE);
+	blist_iterate_action(FALSE);
 }
 
 #if 0
@@ -240,9 +164,6 @@ bit_actions(GaimPlugin *plugin, gpointer context)
 	GList *list = NULL;
 	GaimPluginAction *act = NULL;
 
-	act = gaim_plugin_action_new(_("Compleat Buddy Icon"),
-			compleat_buddy_icon_action);
-	list = g_list_append(list, act);
 #if 0
 /* buddy icon structs currently suck, I think
    it's impossible to tell from a filename which buddy it's associated with
@@ -306,9 +227,14 @@ init_plugin(GaimPlugin *plugin)
 #endif
 	info.name = _("Buddy Icon Tools");
 	info.summary = _("Tools to manipulate buddy icons. *DANGEROUS*");
-	info.description = _("Whilst working on Gaim 2.0.0, I found a need to destroy all my buddies' buddy icons. Also, I finally got annoyed enough that there's no 'Global Buddy Icon', so I thought I'd add that too. There's nothing to do these functions in Gaim, so here they are. Completely, thoroughly untested.");
+	info.description = _("Whilst working on Gaim 2.0.0, I found a need to "
+			"destroy all my buddies' buddy icons. Also, I finally got annoyed "
+			"enough that there's no \"Global Buddy Icon\", so I thought I'd "
+			"add that too. There's nothing to do these functions in Gaim, so "
+			"here they are. Completely, thoroughly untested.");
 
 	bit = plugin; /* handle needed for request API file selector */
 }
 
 GAIM_INIT_PLUGIN(bit, init_plugin, info)
+
