7ff11bc29a7c64cbbcd62fc2a3dbf0d846949989
sadrul <devnull@localhost>
2006-10-14 01:45:25 -0400

    [pp-cvs @ 117] Add a difftopic plugin. Shows the difference between the old topic and the new topic in a chat.

---
+++ difftopic/difftopic.c
@@ -0,0 +1,234 @@
+/*
+ * DiffTopic - Show the old topic when the topic in a chat room changes.
+ * Copyright (C) 2006
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+#if HAVE_CONFIG_H
+# include "../gpp_config.h"
+#endif
+
+#define GAIM_PLUGINS
+
+#define PLUGIN_ID			"gtk-plugin_pack-difftopic"
+#define PLUGIN_NAME			"DiffTopic"
+#define PLUGIN_STATIC_NAME	"DiffTopic"
+#define PLUGIN_SUMMARY		"Show the old topic when the topic in a chat room changes."
+#define PLUGIN_DESCRIPTION	"Show the old topic when the topic in a chat room changes."
+#define PLUGIN_AUTHOR		"Sadrul H Chowdhury <sadrul@users.sourceforge.net>"
+
+/* System headers */
+#include <glib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* Gaim headers */
+#include <plugin.h>
+#include <version.h>
+#include <gtkplugin.h>
+
+#include <conversation.h>
+
+#include <gtkconv.h>
+#include <gtkimhtml.h>
+
+/* Pack/Local headers */
+#include "../common/i18n.h"
+
+#define SAME(a,b)  ((isalnum((a)) && isalnum((b))) || (!isalnum((a)) && !isalnum((b))))
+
+#define SIZE 1000   /* XXX: Let's assume this always holds */
+static int lcs[SIZE][SIZE];
+
+static GList *
+split_string(const char *string)
+{
+	GList *ret = NULL;
+	const char *start, *end;
+
+	start = end = string;
+	while (*start) {
+		while (*end && SAME(*start, *end)) {
+			end++;
+		}
+		ret = g_list_append(ret, g_strndup(start, end - start));
+		start = end;
+	}
+
+	return ret;
+}
+
+static GString *
+g_string_prepend_printf(GString *string, const char *format, ...)
+{
+	va_list args;
+	char *str;
+
+	va_start(args, format);
+	str = g_strdup_vprintf(format, args);
+	va_end(args);
+	string = g_string_prepend(string, str);
+	g_free(str);
+	return string;
+}
+
+/* Let's LCS */
+static void
+have_fun(GtkIMHtml *imhtml, const char *old, const char *new)
+{
+	GList *lold, *lnew;
+	int i, j, m, n;
+	GString *from, *to;
+
+	lold = split_string(old);
+	lnew = split_string(new);
+
+	memset(lcs, 0, sizeof(lcs));
+	n = g_list_length(lold);
+	m = g_list_length(lnew);
+
+	for (i = 1; i <= n; i++)
+		lcs[i][0] = 0;
+
+	for (j = 1; j <= m; j++)
+		lcs[0][j] = 0;
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) {
+			if (strcmp(g_list_nth_data(lold, i-1), g_list_nth_data(lnew, j-1)) == 0)
+				lcs[i][j] = lcs[i-1][j-1] + 1;
+			else
+				lcs[i][j] = (lcs[i-1][j] >= lcs[i][j-1] ? lcs[i-1][j] : lcs[i][j-1]);
+		}
+	}
+
+	from = g_string_new(NULL);
+	to = g_string_new(NULL);
+	i = n;
+	j = m;
+	while (i && j) {
+		if (lcs[i][j] == lcs[i-1][j]) {
+			from = g_string_prepend_printf(from, "<font color='red'><B>%s</B></font>",
+						g_list_nth_data(lold, i - 1));
+			i--;
+		} else if (lcs[i][j] == lcs[i][j-1]) {
+			to = g_string_prepend_printf(to, "<font color='green'><B>%s</B></font>",
+						g_list_nth_data(lnew, j - 1));
+			j--;
+		} else if (lcs[i][j] == lcs[i-1][j-1] + 1) {
+			from = g_string_prepend_printf(from, "<B>%s</B>", g_list_nth_data(lold, i-1));
+			to = g_string_prepend_printf(to, "<B>%s</B>", g_list_nth_data(lnew, j-1));
+			i--;
+			j--;
+		}
+	}
+
+	while (j) {
+		to = g_string_prepend_printf(to, "<font color='green'><B>%s</B></font>",
+					g_list_nth_data(lnew, j - 1));
+		j--;
+	}
+
+	while (i) {
+		from = g_string_prepend_printf(from, "<font color='red'><B>%s</B></font>",
+					g_list_nth_data(lold, i - 1));
+		i--;
+	}
+
+	gtk_imhtml_append_text(GTK_IMHTML(imhtml), "<BR>Topic changed from: <BR>", 0);
+	gtk_imhtml_append_text(GTK_IMHTML(imhtml), from->str, 0);
+	gtk_imhtml_append_text(GTK_IMHTML(imhtml), "<BR>To:<BR>", 0);
+	gtk_imhtml_append_text(GTK_IMHTML(imhtml), to->str, 0);
+
+	g_string_free(from, TRUE);
+	g_string_free(to, TRUE);
+	g_list_foreach(lold, (GFunc)g_free, NULL);
+	g_list_foreach(lnew, (GFunc)g_free, NULL);
+	g_list_free(lold);
+	g_list_free(lnew);
+}
+
+static void
+topic_changed(GaimConversation *conv, const char *who, const char *what)
+{
+	GaimGtkConversation *gtkconv = conv->ui_data;
+	char *old;
+
+	old = g_object_get_data(G_OBJECT(gtkconv->imhtml), "difftopic");
+	if (old) {
+		have_fun(GTK_IMHTML(gtkconv->imhtml), old, what);
+	}
+	g_object_set_data_full(G_OBJECT(gtkconv->imhtml), "difftopic", g_strdup(what), (GDestroyNotify)g_free);
+}
+
+static gboolean
+plugin_load(GaimPlugin *plugin)
+{
+	gaim_signal_connect(gaim_conversations_get_handle(), "chat-topic-changed",
+				plugin, GAIM_CALLBACK(topic_changed), NULL);
+	/* Set the topic to the opened chat windows */
+	return TRUE;
+}
+
+static gboolean
+plugin_unload(GaimPlugin *plugin)
+{
+	return TRUE;
+}
+
+static GaimPluginInfo info =
+{
+	GAIM_PLUGIN_MAGIC,    /* Magic              */
+	GAIM_MAJOR_VERSION,   /* Gaim Major Version */
+	GAIM_MINOR_VERSION,   /* Gaim Minor Version */
+	GAIM_PLUGIN_STANDARD, /* plugin type        */
+	GAIM_GTK_PLUGIN_TYPE, /* ui requirement     */
+	0,                    /* flags              */
+	NULL,                 /* dependencies       */
+	GAIM_PRIORITY_DEFAULT,/* priority           */
+
+	PLUGIN_ID,            /* plugin id          */
+	NULL,                 /* name               */
+	GPP_VERSION,          /* version            */
+	NULL,                 /* summary            */
+	NULL,                 /* description        */
+	PLUGIN_AUTHOR,        /* author             */
+	GPP_WEBSITE,          /* website            */
+
+	plugin_load,          /* load               */
+	plugin_unload,        /* unload             */
+	NULL,                 /* destroy            */
+
+	NULL,                 /* ui_info            */
+	NULL,                 /* extra_info         */
+	NULL,                 /* prefs_info         */
+	NULL                  /* actions            */
+};
+
+static void
+init_plugin(GaimPlugin *plugin)
+{
+#ifdef ENABLE_NLS
+	bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR);
+	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+#endif /* ENABLE_NLS */
+
+	info.name = _(PLUGIN_NAME);
+	info.summary = _(PLUGIN_SUMMARY);
+	info.description = _(PLUGIN_DESCRIPTION);
+}
+
+GAIM_INIT_PLUGIN(PLUGIN_STATIC_NAME, init_plugin, info)
