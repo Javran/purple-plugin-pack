29e3da804bbef2eaf4970776292e42f8345f615c
rekkanoryo <rekkanoryo@guifications.org>
2007-04-15 04:55:23 -0400

    A better fix since pidgin.h is missing a define that gaim.h used to have

--- sepandtab/sepandtab.c
+++ sepandtab/sepandtab.c
@@ -21,19 +21,24 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 #ifdef HAVE_CONFIG_H
-# include "../gpp_config.h"
+# include "../pp_config.h"
 #endif
 
 #define PURPLE_PLUGINS
 
 #include <conversation.h>
 #include <plugin.h>
+#include <pidgin.h>
 #include <version.h>
 #include <gtkplugin.h>
 #include <gtkconv.h>
 #include <gtkconvwin.h>
 
 #include "../common/i18n.h"
+
+/* XXX: THIS MUST BE CHANGED WHEN PIDGIN DOES ITS PREFS MIGRATION!!!!! */
+#define SEPANDTAB_PREF "/gaim/gtk/conversations/placement"
+
 
 static void
 conv_placement_sep_ims_tab_chats(PidginConversation *conv) {
@@ -91,7 +96,7 @@ plugin_load(PurplePlugin *plugin) {
 								&conv_placement_sep_chats_tab_ims);
 	pidgin_conv_placement_add_fnc("grp-type-sep-prpl",	_("Group by Type, Separate by Protocol"),
 								&conv_placement_grp_type_sep_prpl);
-	purple_prefs_trigger_callback(PIDGIN_PREFS_ROOT "/conversations/placement");
+	purple_prefs_trigger_callback(SEPANDTAB_PREF);
 
 	return TRUE;
 }
@@ -99,9 +104,10 @@ plugin_load(PurplePlugin *plugin) {
 static gboolean
 plugin_unload(PurplePlugin *plugin) {
 	pidgin_conv_placement_remove_fnc("sep-ims-tab-chats");
+
 	pidgin_conv_placement_remove_fnc("sep-chats-tab-ims");
 
-	purple_prefs_trigger_callback(PIDGIN_PREFS_ROOT "/conversations/placement");
+	purple_prefs_trigger_callback(SEPANDTAB_PREF);
 
 	return TRUE;
 }
@@ -117,11 +123,11 @@ static PurplePluginInfo info = {
 	PURPLE_PRIORITY_DEFAULT,
 	"gtk-plugin_pack-separate_and_tab",
 	NULL,
-	GPP_VERSION,
+	PP_VERSION,
 	NULL,
 	NULL,
 	"Gary Kramlich <amc_grim@users.sf.net>",
-	GPP_WEBSITE,
+	PP_WEBSITE,
 	plugin_load,
 	plugin_unload,
 	NULL,
