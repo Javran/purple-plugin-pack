e6209e1ed56b0c2bcb44d99ef380a9060aa57314
grim <grim@guifications.org>
2008-03-29 18:50:25 -0400

    Renamed plugins.ini to plugins.cfg Changed the format a bit, renamed dependencies to depends, added a provides option, and remove the plugins section Added plugins.cfg for every directory that contains at least one plugin

--- pluginpack.py
+++ pluginpack.py
@@ -5,63 +5,81 @@ import glob
 import os.path
 import sys
 
-plugins = []
+def printerr(msg):
+	print >> sys.stderr, msg
 
 class Plugin:
 	name = ''
+	provides = ''
 	summary = ''
 	description = ''
 	directory = ''
-	dependencies = []
+	depends = []
 	type = ''
 
 	def __init__(self, directory, name, parser):
 		self.directory = directory
+		self.name = name
 
-		self.name = parser.get(name, 'name')
+		self.provides = parser.get(name, 'provides')
 		self.summary = parser.get(name, 'summary')
 		self.description = parser.get(name, 'description')
-		self.dependencies = parser.get(name, 'dependencies').split()
+		self.depends = parser.get(name, 'depends').split()
 		self.type = parser.get(name, 'type')
 
+		if self.type != 'default' and self.type != 'incomplete' and self.type != 'abusive':
+			printerr('\'%s\' has an unknown type of \'%s\'!' % (self.name, self.type))
+
 	def __str__(self):
-		return '[name=\'%s\', directory=\'%s\']' % (self.name, self.directory)
+		return self.name
 
-def printerr(msg):
-	print >> sys.stderr, msg
+class PluginPack:
+	plugins = []
 
-def load_plugins():
-	for file in glob.glob('*/plugins.ini'):
-		parser = ConfigParser.ConfigParser()
+	def load_plugins(self):
+		for file in glob.glob('*/plugins.cfg'):
+			parser = ConfigParser.ConfigParser()
 
-		try:
-			parser.read(file)
-		except ConfigParser.ParsingError,  msg:
-			printerr('Failed to parse \'%s\':\n%s' % (file, msg))
-			continue
-
-		if not parser.has_section('plugins'):
-			printerr('\'%s\' does not have a plugins section!' % file)
-			continue
-
-		if not parser.has_option('plugins', 'sections'):
-			printerr('\'%s\' does not have a sections option under the plugins section' % file)
-			continue
-
-		for plugin in parser.get('plugins', 'sections').split():
-			if not parser.has_section(plugin):
-				printerr('\'%s\' does not have a section named \'%s\'!' % (file, plugin))
+			try:
+				parser.read(file)
+			except ConfigParser.ParsingError,  msg:
+				printerr('Failed to parse \'%s\':\n%s' % (file, msg))
 				continue
 
-			p = Plugin(os.path.dirname(file), plugin, parser)
+			for plugin in parser.sections():
+				p = Plugin(os.path.dirname(file), plugin, parser)
 
-			plugins.append(p)
+				self.plugins.append(p)
+
+	def list(self, type):
+		list = []
+
+		for plugin in self.plugins:
+			if plugin.type == type:
+				list.append(plugin)
+
+		return list
+
+	def dump_list(self, list):
+		for i in list:
+			print '\t%s' % i
+
+	def debug(self):
+		print 'Default:'
+		self.dump_list(self.list('default'))
+
+		print 'Abusive:'
+		self.dump_list(self.list('abusive'))
+
+		print 'Incomplete:'
+		self.dump_list(self.list('incomplete'))
 
 def main():
-	load_plugins()
+	pp = PluginPack()
 
-	for p in plugins:
-		print p
+	pp.load_plugins()
+
+	pp.debug()
 
 if __name__ == "__main__":
 	main()
