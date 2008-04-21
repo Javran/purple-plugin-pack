9c3f08c2855608503be33ebc9d2b3d841b6ccef5
grim <grim@guifications.org>
2008-04-21 02:48:37 -0400

    Fixed output for failed run_or_die's Added stu's fix for making which quieter

--- autogen.sh
+++ autogen.sh
@@ -55,7 +55,7 @@ check () {
 	CMD=$1
 
 	echo -n "checking for ${CMD}... "
-	BIN=`which ${CMD}`
+	BIN=`which ${CMD} 2>/dev/null`
 
 	if [ x"${BIN}" = x"" ] ; then
 		echo "not found."
@@ -70,17 +70,23 @@ run_or_die () { # beotch
 	CMD=$1
 	shift
 
+	OUTPUT=`mktemp autogen-XXXX`
+
 	echo -n "running ${CMD} ${@}... "
-	OUTPUT=`${CMD} ${@} 2>&1`
+	${CMD} ${@} >${OUTPUT} 2>&1
+
 	if [ $? != 0 ] ; then
 		echo "failed."
-		echo ${OUTPUT}
+		cat ${OUTPUT}
+		rm -f ${OUTPUT}
 		exit 1
 	else
 		echo "done."
-		if [ x"${OUTPUT}" != x"" ] ; then
-			echo ${OUTPUT}
+		if [ `stat --printf="%s" ${OUTPUT}` -ge 0 ] ; then
+			cat ${OUTPUT}
 		fi
+		
+		rm -f ${OUTPUT}
 	fi
 }
 
