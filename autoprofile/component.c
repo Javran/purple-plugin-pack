16e3e36cdb8e2804a94f2ae0734aefa229cbb0c7
Paul Aurich <paul@darkrain42.org>
2009-07-24 21:59:52 -0700

    Add a comment for the other warning, which is a valid error.

--- autoprofile/component.c
+++ autoprofile/component.c
@@ -66,6 +66,7 @@ static GList *get_components ()
   /* Add each listed component */
 
   /*
+  XXX BEFORE YOU UNCOMMENT THIS, FIX THE IDENTIFIERS in logstats */
   ret = g_list_append (ret, &logstats);
   */
 
