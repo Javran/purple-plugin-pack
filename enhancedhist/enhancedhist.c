455f322e6f9b9eae834d3222a19f2418959a94d2
Paul Aurich <paul@darkrain42.org>
2009-10-25 21:04:22 -0700

    Fix indentation

--- enhancedhist/enhancedhist.c
+++ enhancedhist/enhancedhist.c
@@ -153,11 +153,10 @@ static void historize(PurpleConversation *c)
 			}
 		}
 
-			if(logs)
-				logs = g_list_sort(logs, purple_log_compare);
-			else
-				logs = purple_log_get_logs(PURPLE_LOG_IM, name, account);
-
+		if (logs)
+			logs = g_list_sort(logs, purple_log_compare);
+		else
+			logs = purple_log_get_logs(PURPLE_LOG_IM, name, account);
 	} else if (convtype == PURPLE_CONV_TYPE_CHAT && PREF_CHAT_VAL) {
 		logs = purple_log_get_logs(PURPLE_LOG_CHAT,
 			purple_conversation_get_name(c), purple_conversation_get_account(c));
